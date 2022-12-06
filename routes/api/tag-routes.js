c7d751be24ed27b3cc6e030b20be875bfa74d566
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-05 17:19:48 -0700

    routes->api->tag-routes added in the post, put and delete routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,14 +38,31 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value use destroy to make it meet certain criteria to delete
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
