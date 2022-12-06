a4da7f2ee0e614de77bd8883d84bf944ca0caf55
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-05 17:03:03 -0700

    routes->api->product-routes.js added in the delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,6 +116,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => res.status(200).json(product))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
